693b4fd2127a8a1e3c0b05432dd250a5f9300991
kazukimastumoto <uver.loveloveleke@icloud.com>
2020-11-10 16:05:07 +0900

    投稿機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,25 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.order('created_at DESC')
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:image, :price, :title, :body).merge(user_id: current_user.id)
   end
 end
